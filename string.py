d1423918d86eb61b56356b91132162b69b7d2e32
rajesh <rajeshsuper789@gmail.com>
2023-09-16 19:12:39 +0530

    string

---
+++ string.py
@@ -0,0 +1,8 @@
+message='this is rajesh'
+print("\n",message)
+message='this rajesh'
+print("\n",message)
+message="it's my pen"
+print("\n",message)
+message='"hia this is rajesh." i am in anantapur'
+print("\n",message)
