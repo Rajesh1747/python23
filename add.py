6c8bde3cdd72af22a5ba03099a9d10f5b1354281
rajesh <rajeshsuper789@gmail.com>
2023-09-16 18:16:46 +0530

    python

--- add.py
+++ add.py
@@ -3,4 +3,4 @@ b=30
 c=40
 d=1000
 e=a+b*c-d 
-print("sum of the four values are",e)
+print("result",e) 
---
+++ commend.py
@@ -0,0 +1,7 @@
+a=10 # the first value
+b=20 # the second value
+c=30 # the third value
+#calculation part
+d=a+b+c
+# result part 
+print("sum of the three results are ",d)
