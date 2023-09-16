d5c174c531ccef1547075f0a6c1f4dd7f0c21e91
rajesh <rajeshsuper789@gmail.com>
2023-09-16 19:03:19 +0530

    intfloat

---
+++ intfloatchar.py
@@ -0,0 +1,7 @@
+a=20
+b=2.34
+message="this is a message for python"
+
+print("\n given value is in integer format",a) 
+print("\n given value is in float formate",b)
+print("\n given text message in char formate",message)
