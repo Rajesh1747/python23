677f7ed9eefa444422fbd12850de921ff935e047
rajesh <rajeshsuper789@gmail.com>
2023-09-19 10:46:54 +0530

    all-maths

---
+++ all-maths-files.py
@@ -0,0 +1,18 @@
+a=20
+b=30 
+c=40 
+d=50 
+
+add=a+b 
+sub=b-c 
+div=d/c 
+mul=c*b
+muld=a%c 
+pow=d**c 
+
+print("\n the addition of two value ",add)
+print("\n the substraction of two value",sub)
+print("\n the multiplication of two value",mul)
+print("\n the division  of two value",div)
+print("\n the mud of two value",muld)
+print("\n the pow of two value",pow)
