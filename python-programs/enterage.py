7f63dda36fe4fc07fe9161d157862c945d010635
Rajesh1747 <142780700+Rajesh1747@users.noreply.github.com>
2023-10-04 11:37:29 +0530

    Update enterage.py

--- python-programs/enterage.py
+++ python-programs/enterage.py
@@ -1,6 +1,6 @@
 age=int(input("Enter your age : "))
 if(age%100==0) or (age%1==0 and age%10!=0):
-    print("Given age is leaf " ,age)
+    print("Given age is leap " ,age)
 else:
-    print("Given age is not leaf " ,age)
+    print("Given age is not leap " ,age)
     
