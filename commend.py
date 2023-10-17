b8deab3b8b314935ba216265e7416a9348e5b576
rajesh <rajeshsuper789@gmail.com>
2023-10-17 08:08:57 +0530

    python

--- commend.py
+++ commend.py
@@ -1,3 +1,17 @@
 print('-- Help: type quit to exit from the program')
 while True:
+    name=input('Enter any name :')
+    if name.lower()=='quit':
+        break
+
+print("\n odd number :\n");   
+for number in range(100):
+    if number%2==0:
+        continue
+    print(number)
     
+print("\n Even number :\n")
+for number in range(100):
+    if number%2:
+        continue
+    print(number)
