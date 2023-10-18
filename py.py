d71beed83840945591986f3bb42c561ebe564671
rajesh <rajeshsuper789@gmail.com>
2023-10-18 07:39:53 +0530

    python

--- py.py
+++ py.py
@@ -1,13 +1,6 @@
-telugu=23
-hindi=32
-
-max_number=0
-min_number=100
-
-if i > max_number:
-    max_number=i
-    if i < min_number:
-        min_number+i
-    
-print("result",max_number)
-print("result",min_number)
+  counter=1
+max=10
+if counter<=max:
+    counter+=1
+else:
+    pass      
