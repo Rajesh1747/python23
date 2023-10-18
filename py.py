ac9199a0b3a2baed0b3bc171d50c312307de87cb
rajesh <rajeshsuper789@gmail.com>
2023-10-18 08:15:02 +0530

    python

--- py.py
+++ py.py
@@ -1,6 +1,25 @@
-  counter=1
-max=10
-if counter<=max:
-    counter+=1
-else:
-    pass      
+#by singel quotes
+message='Hi this is Sandy'
+print("THis is double quotes message",message)  
+
+
+#by Double quotes
+message="Hi this is Sandy"
+print("THis is double quotes message ",message)  
+
+#Text in quotes on output 
+message="Hi this is Sandy"
+print(message)
+
+#multiline Text message 
+message='''
+hi
+this is rajesh'''
+print(message)
+
+
+#string concatenation
+string1='hi'
+string2='good morning'
+greeting=string1+string2
+print(greeting)
