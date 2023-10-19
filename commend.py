656554020fb3ec9c237ae4d3576a016237275264
rajesh <rajeshsuper789@gmail.com>
2023-10-19 10:45:52 +0530

    python

--- commend.py
+++ commend.py
@@ -1,17 +1,5 @@
-print('-- Help: type quit to exit from the program')
-while True:
-    name=input('Enter any name :')
-    if name.lower()=='quit':
-        break
-
-print("\n odd number :\n")
-for number in range(10):
-    if number%2==0:
-        continue
-    print(number)
-    
-print("\n Even number :\n")
-for number in range(10):
-    if number%2:
-        continue
-    print(number)
+#To find given String length
+str="Clpud Gensoftech"
+str_len=len(str)
+print(" Given Cloud GEnsoftech String length is ",str_len)
+print("\n\n\n\n\n");
---
+++ string1.py
@@ -0,0 +1,6 @@
+str="python string"
+print(str[1])
+print(str[3])
+print(str[8])
+print(str[2])
+
