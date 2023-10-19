a24d38a39195c6f66db04ed3ca9fc85be27d3ccd
rajesh <rajeshsuper789@gmail.com>
2023-10-19 10:48:54 +0530

    python

--- py.py
+++ py.py
@@ -1,25 +1,6 @@
-#by singel quotes
-message='Hi this is Sandy'
-print("THis is double quotes message",message)  
-
-
-#by Double quotes
-message="Hi this is Sandy"
-print("THis is double quotes message ",message)  
-
-#Text in quotes on output 
-message="Hi this is Sandy"
-print(message)
-
-#multiline Text message 
-message='''
-hi
-this is rajesh'''
-print(message)
-
-
-#string concatenation
-string1='hi'
-string2='good morning'
-greeting=string1+string2
-print(greeting)
+#string Length by len Function 
+str=input("Enter the name : ")
+print(str[1])
+print(str[3])
+print(str[4])
+print(str[2])
