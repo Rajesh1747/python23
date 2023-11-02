e97405a8d7ffacfe9ccfe52d209fa88772873e0c
rajesh <rajeshsuper789@gmail.com>
2023-11-02 17:07:19 +0530

    tuple python

--- commend.py
+++ commend.py
@@ -1,5 +1,5 @@
 #To find given String length
 str="Clpud Gensoftech"
 str_len=len(str)
-print(" Given Cloud GEnsoftech String length is ",str_len)
+print(" Given Cloud Gensoftech String length is ",str_len)
 print("\n\n\n\n\n");
---
+++ tuple.py
@@ -0,0 +1,7 @@
+guests=[1,2,3,4,5,6,7,8,9]
+guests.sort(reverse=True)
+print(guests)
+
+guests=[2,4,6,8,1,3,0]
+guests.sort()
+print(guests)
